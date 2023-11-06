c26673ed96147fbc5304af049fea5df03f37d5ee
Hamsa Jama <hamsa.jama@cazoo.co.uk>
2023-11-06 23:47:41 +0000

    add filtering and gradients

--- image_processing/core.py
+++ image_processing/core.py
@@ -1,5 +1,12 @@
-from optimaimg import convert_to_grayscale as _convert_to_grayscale
-from optimaimg import resize_image as _resize_image
+from optimaimg import (
+    convert_to_grayscale as _convert_to_grayscale,
+    resize_image as _resize_image,
+    rotate_image as _rotate_image,
+    apply_blur as _apply_blur,
+    apply_sharpen as _apply_sharpen,
+    apply_edge_detection as _apply_edge_detection,
+    apply_sepia as _apply_sepia,
+)
 
 
 def convert_to_grayscale(input_path, output_path):
@@ -10,3 +17,28 @@ def convert_to_grayscale(input_path, output_path):
 def resize_image(input_path, output_path, width, height):
     # You can add additional Python logic here if needed
     return _resize_image(input_path, output_path, width, height)
+
+
+def rotate_image(input_path, output_path, degrees):
+    # You can add additional Python logic here if needed
+    return _rotate_image(input_path, output_path, degrees)
+
+
+def apply_blur(input_path, output_path):
+    # You can add additional Python logic here if needed
+    return _apply_blur(input_path, output_path)
+
+
+def apply_sharpen(input_path, output_path):
+    # You can add additional Python logic here if needed
+    return _apply_sharpen(input_path, output_path)
+
+
+def apply_edge_detection(input_path, output_path):
+    # You can add additional Python logic here if needed
+    return _apply_edge_detection(input_path, output_path)
+
+
+def apply_sepia(input_path, output_path):
+    # You can add additional Python logic here if needed
+    return _apply_sepia(input_path, output_path)
--- src/lib.rs
+++ src/lib.rs
@@ -1,103 +1,103 @@
-use image::ImageBuffer;
+use image::{
+    DynamicImage, ImageBuffer, ImageFormat, Luma, Rgba, open, imageops::FilterType,
+};
+use imageproc::{
+    filter::{gaussian_blur_f32, sharpen3x3},
+    geometric_transformations::{rotate_about_center, Interpolation},
+    gradients::sobel_gradients,
+    map::map_colors,
+};
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
-use image::{open, ImageFormat, Rgba, imageops::FilterType};
-use imageproc::geometric_transformations::{rotate_about_center, Interpolation};
+use std::path::Path;
 
+const TRANSPARENT_COLOR: Rgba<u8> = Rgba([0, 0, 0, 0]);
+
+// Generic function to save any image buffer with a specified format
+fn save_image<T>(image: &ImageBuffer<T, Vec<u8>>, output_path: &str, image_format: ImageFormat) -> PyResult<()>
+where
+    T: image::Pixel<Subpixel = u8> + 'static + image::PixelWithColorType,
+{
+    image
+        .save_with_format(output_path, image_format)
+        .map_err(|e| PyErr::new::<pyo3::exceptions::PyIOError, _>(format!("Failed to save image: {}", e)))
+}
+
+// Generic function to open an image from a file path
+fn open_image(path: &str) -> PyResult<DynamicImage> {
+    open(Path::new(path))
+        .map_err(|e| PyErr::new::<pyo3::exceptions::PyIOError, _>(format!("Failed to open image: {}", e)))
+}
 
 #[pyfunction]
 fn resize_image(input_path: String, output_path: String, width: u32, height: u32) -> PyResult<()> {
-    match open(&input_path) {
-        Ok(img) => {
-            // Resize the image to the specified dimensions using the Lanczos3 filter
-            let resized_img = img.resize_exact(width, height, FilterType::Lanczos3);
-
-            // Save the resized image to the output path
-            match resized_img.save_with_format(&output_path, ImageFormat::Png) {
-                Ok(_) => {
-                    println!("Resized image and saved to {}", output_path);
-                    Ok(())
-                },
-                Err(e) => {
-                    println!("Failed to save resized image: {}", e);
-                    Err(PyErr::new::<pyo3::exceptions::PyIOError, _>(format!("Failed to save resized image: {}", e)))
-                }
-            }
-        },
-        Err(e) => {
-            println!("Failed to open image: {}", e);
-            Err(PyErr::new::<pyo3::exceptions::PyIOError, _>(format!("Failed to open image: {}", e)))
-        }
-    }
+    let img = open_image(&input_path)?;
+    let resized_img = img.resize_exact(width, height, FilterType::Lanczos3);
+    save_image(&resized_img.to_rgba8(), &output_path, ImageFormat::Png)
 }
 
 #[pyfunction]
 fn convert_to_grayscale(input_path: String, output_path: String) -> PyResult<()> {
-
-    match open(&input_path) {
-        Ok(img) => {
-            let grayscale_img = img.to_luma8();
-
-            match grayscale_img.save_with_format(&output_path, ImageFormat::Png) {
-                Ok(_) => {
-                    println!("Converted image to grayscale and saved to {}", output_path);
-                    Ok(())
-                },
-                Err(e) => {
-                    println!("Failed to save grayscale image: {}", e);
-                    Err(PyErr::new::<pyo3::exceptions::PyIOError, _>(format!("Failed to save grayscale image: {}", e)))
-                }
-            }
-        },
-        Err(e) => {
-            println!("Failed to open image: {}", e);
-            Err(PyErr::new::<pyo3::exceptions::PyIOError, _>(format!("Failed to open image: {}", e)))
-        }
-    }
+    let img = open_image(&input_path)?;
+    save_image(&img.to_luma8(), &output_path, ImageFormat::Png)
 }
 
 #[pyfunction]
 fn rotate_image(input_path: String, output_path: String, degrees: f32) -> PyResult<()> {
-    // Open the image from the provided input path
-    match open(&input_path) {
-        Ok(dyn_img) => {
-            // Convert the DynamicImage to an ImageBuffer of Rgba<u8>
-            let img_buffer = dyn_img.to_rgba8();
-
-            // Convert degrees to radians and cast to f32 as required by rotate_about_center
-            let radians = degrees.to_radians();
-
-            // Rotate the image about its center
-            let rotated_img: ImageBuffer<Rgba<u8>, Vec<u8>> = rotate_about_center(
-                &img_buffer,
-                radians,
-                Interpolation::Bilinear, // You can choose Nearest, Bilinear, or Cubic
-                Rgba([0, 0, 0, 0]) // Set to fully transparent or another color of your choice
-            );
-
-            // Save the rotated image to the output path
-            match rotated_img.save_with_format(&output_path, ImageFormat::Png) {
-                Ok(_) => {
-                    println!("Rotated image and saved to {}", output_path);
-                    Ok(())
-                },
-                Err(e) => {
-                    println!("Failed to save rotated image: {}", e);
-                    Err(PyErr::new::<pyo3::exceptions::PyIOError, _>(format!("Failed to save rotated image: {}", e)))
-                }
-            }
-        },
-        Err(e) => {
-            println!("Failed to open image: {}", e);
-            Err(PyErr::new::<pyo3::exceptions::PyIOError, _>(format!("Failed to open image: {}", e)))
-        }
-    }
+    let dyn_img = open_image(&input_path)?;
+    let img_buffer = dyn_img.to_rgba8();
+    let radians = degrees.to_radians();
+    let rotated_img = rotate_about_center(&img_buffer, radians, Interpolation::Bilinear, TRANSPARENT_COLOR);
+    save_image(&rotated_img, &output_path, ImageFormat::Png)
 }
 
+#[pyfunction]
+fn apply_blur(input_path: String, output_path: String, sigma: f32) -> PyResult<()> {
+    let img = open_image(&input_path)?;
+    save_image(&gaussian_blur_f32(&img.to_rgba8(), sigma), &output_path, ImageFormat::Png)
+}
+
+#[pyfunction]
+fn apply_sharpen(input_path: String, output_path: String) -> PyResult<()> {
+    let img = open_image(&input_path)?;
+    save_image(&sharpen3x3(&img.to_luma8()), &output_path, ImageFormat::Png)
+}
+
+#[pyfunction]
+fn apply_edge_detection(input_path: String, output_path: String) -> PyResult<()> {
+    let img = open_image(&input_path)?;
+    let edge_detected_image_u16 = sobel_gradients(&img.to_luma8());
+
+    let mut edge_detected_image_u8 = ImageBuffer::new(edge_detected_image_u16.width(), edge_detected_image_u16.height());
+    for (x, y, pixel) in edge_detected_image_u16.enumerate_pixels() {
+        let val = pixel[0] as u32;
+        let scaled_val = (val * 255) / u16::MAX as u32;
+        edge_detected_image_u8.put_pixel(x, y, Luma([scaled_val as u8]));
+    }
+    save_image(&edge_detected_image_u8, &output_path, ImageFormat::Png)
+}
+
+#[pyfunction]
+fn apply_sepia(input_path: String, output_path: String) -> PyResult<()> {
+    let img = open_image(&input_path)?;
+    let sepia_image = map_colors(&img.to_rgba8(), |p| {
+        let r = (p[0] as f32 * 0.393) + (p[1] as f32 * 0.769) + (p[2] as f32 * 0.189);
+        let g = (p[0] as f32 * 0.349) + (p[1] as f32 * 0.686) + (p[2] as f32 * 0.168);
+        let b = (p[0] as f32 * 0.272) + (p[1] as f32 * 0.534) + (p[2] as f32 * 0.131);
+        Rgba([r.min(255.0) as u8, g.min(255.0) as u8, b.min(255.0) as u8, p[3]])
+    });
+    save_image(&sepia_image, &output_path, ImageFormat::Png)
+}
+
+// Add all the pyfunctions to the Python module
 #[pymodule]
 fn optimaimg(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(convert_to_grayscale, m)?)?;
     m.add_function(wrap_pyfunction!(resize_image, m)?)?;
+    m.add_function(wrap_pyfunction!(convert_to_grayscale, m)?)?;
     m.add_function(wrap_pyfunction!(rotate_image, m)?)?;
+    m.add_function(wrap_pyfunction!(apply_blur, m)?)?;
+    m.add_function(wrap_pyfunction!(apply_sharpen, m)?)?;
+    m.add_function(wrap_pyfunction!(apply_edge_detection, m)?)?;
+    m.add_function(wrap_pyfunction!(apply_sepia, m)?)?;
     Ok(())
 }
