b11ed67f383ea90ae83316a004111c0f85ad8a3a
Hamsa Jama <hamsa.jama@cazoo.co.uk>
2023-11-06 22:34:31 +0000

    add rotate image feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
+use image::ImageBuffer;
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
-use image::{open, ImageFormat, imageops::FilterType};
+use image::{open, ImageFormat, Rgba, imageops::FilterType};
+use imageproc::geometric_transformations::{rotate_about_center, Interpolation};
+
 
 #[pyfunction]
 fn resize_image(input_path: String, output_path: String, width: u32, height: u32) -> PyResult<()> {
@@ -53,10 +56,48 @@ fn convert_to_grayscale(input_path: String, output_path: String) -> PyResult<()>
     }
 }
 
+#[pyfunction]
+fn rotate_image(input_path: String, output_path: String, degrees: f32) -> PyResult<()> {
+    // Open the image from the provided input path
+    match open(&input_path) {
+        Ok(dyn_img) => {
+            // Convert the DynamicImage to an ImageBuffer of Rgba<u8>
+            let img_buffer = dyn_img.to_rgba8();
+
+            // Convert degrees to radians and cast to f32 as required by rotate_about_center
+            let radians = degrees.to_radians();
+
+            // Rotate the image about its center
+            let rotated_img: ImageBuffer<Rgba<u8>, Vec<u8>> = rotate_about_center(
+                &img_buffer,
+                radians,
+                Interpolation::Bilinear, // You can choose Nearest, Bilinear, or Cubic
+                Rgba([0, 0, 0, 0]) // Set to fully transparent or another color of your choice
+            );
+
+            // Save the rotated image to the output path
+            match rotated_img.save_with_format(&output_path, ImageFormat::Png) {
+                Ok(_) => {
+                    println!("Rotated image and saved to {}", output_path);
+                    Ok(())
+                },
+                Err(e) => {
+                    println!("Failed to save rotated image: {}", e);
+                    Err(PyErr::new::<pyo3::exceptions::PyIOError, _>(format!("Failed to save rotated image: {}", e)))
+                }
+            }
+        },
+        Err(e) => {
+            println!("Failed to open image: {}", e);
+            Err(PyErr::new::<pyo3::exceptions::PyIOError, _>(format!("Failed to open image: {}", e)))
+        }
+    }
+}
+
 #[pymodule]
 fn optimaimg(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(convert_to_grayscale, m)?)?;
     m.add_function(wrap_pyfunction!(resize_image, m)?)?;
-    
+    m.add_function(wrap_pyfunction!(rotate_image, m)?)?;
     Ok(())
 }
