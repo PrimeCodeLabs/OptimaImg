d394be331783f686f92b35bdd9127e8bc32944dd
Hamsa Jama <hamsa.jama@cazoo.co.uk>
2023-11-07 07:36:42 +0000

    fixed an issue with sharpen and edge-detection

--- image_processing/core.py
+++ image_processing/core.py
@@ -9,36 +9,103 @@ from optimaimg import (
 )
 
 
-def convert_to_grayscale(input_path, output_path):
-    # You can add additional Python logic here if needed
+def convert_to_grayscale(input_path: str, output_path: str) -> None:
+    """
+    Convert an image to grayscale.
+
+    Parameters:
+    - input_path (str): The path to the input image.
+    - output_path (str): The path where the grayscale image will be saved.
+
+    Returns:
+    - None: The result of the Rust function, typically None if successful.
+    """
     return _convert_to_grayscale(input_path, output_path)
 
 
-def resize_image(input_path, output_path, width, height):
-    # You can add additional Python logic here if needed
+def resize_image(input_path: str, output_path: str, width: int, height: int) -> None:
+    """
+    Resize an image to the specified dimensions.
+
+    Parameters:
+    - input_path (str): The path to the input image.
+    - output_path (str): The path where the resized image will be saved.
+    - width (int): The desired width of the image.
+    - height (int): The desired height of the image.
+
+    Returns:
+    - None: The result of the Rust function, typically None if successful.
+    """
     return _resize_image(input_path, output_path, width, height)
 
 
-def rotate_image(input_path, output_path, degrees):
-    # You can add additional Python logic here if needed
+def rotate_image(input_path: str, output_path: str, degrees: float) -> None:
+    """
+    Rotate an image by the specified number of degrees.
+
+    Parameters:
+    - input_path (str): The path to the input image.
+    - output_path (str): The path where the rotated image will be saved.
+    - degrees (float): The angle in degrees to rotate the image.
+
+    Returns:
+    - None: The result of the Rust function, typically None if successful.
+    """
     return _rotate_image(input_path, output_path, degrees)
 
 
-def apply_blur(input_path, output_path):
-    # You can add additional Python logic here if needed
-    return _apply_blur(input_path, output_path)
+def apply_blur(input_path: str, output_path: str, sigma: float) -> None:
+    """
+    Apply Gaussian blur to an image.
+
+    Parameters:
+    - input_path (str): The path to the input image.
+    - output_path (str): The path where the blurred image will be saved.
+    - sigma (float): The sigma parameter for the Gaussian function, determining the amount of blur.
+
+    Returns:
+    - None: The result of the Rust function, typically None if successful.
+    """
+    return _apply_blur(input_path, output_path, sigma)
 
 
-def apply_sharpen(input_path, output_path):
-    # You can add additional Python logic here if needed
+def apply_sharpen(input_path: str, output_path: str) -> None:
+    """
+    Sharpen an image.
+
+    Parameters:
+    - input_path (str): The path to the input image.
+    - output_path (str): The path where the sharpened image will be saved.
+
+    Returns:
+    - None: The result of the Rust function, typically None if successful.
+    """
     return _apply_sharpen(input_path, output_path)
 
 
-def apply_edge_detection(input_path, output_path):
-    # You can add additional Python logic here if needed
+def apply_edge_detection(input_path: str, output_path: str) -> None:
+    """
+    Apply edge detection to an image.
+
+    Parameters:
+    - input_path (str): The path to the input image.
+    - output_path (str): The path where the edge-detected image will be saved.
+
+    Returns:
+    - None: The result of the Rust function, typically None if successful.
+    """
     return _apply_edge_detection(input_path, output_path)
 
 
-def apply_sepia(input_path, output_path):
-    # You can add additional Python logic here if needed
+def apply_sepia(input_path: str, output_path: str) -> None:
+    """
+    Apply a sepia tone to an image.
+
+    Parameters:
+    - input_path (str): The path to the input image.
+    - output_path (str): The path where the sepia-toned image will be saved.
+
+    Returns:
+    - None: The result of the Rust function, typically None if successful.
+    """
     return _apply_sepia(input_path, output_path)
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,17 @@
+// Import necessary structs, traits, and functions from the image and imageproc crates
 use image::{
-    DynamicImage, ImageBuffer, ImageFormat, Luma, Rgba, open, imageops::FilterType,
+    DynamicImage, ImageBuffer, ImageFormat, Luma, Rgba, RgbImage, Rgb, open,
+    imageops::FilterType
 };
 use imageproc::{
-    filter::{gaussian_blur_f32, sharpen3x3},
+    filter::gaussian_blur_f32,
     geometric_transformations::{rotate_about_center, Interpolation},
     gradients::sobel_gradients,
     map::map_colors,
 };
-use pyo3::prelude::*;
+use pyo3::{prelude::*, exceptions::PyIOError};
 use pyo3::wrap_pyfunction;
-use std::path::Path;
+use std::{path::Path, convert::TryInto};
 
 const TRANSPARENT_COLOR: Rgba<u8> = Rgba([0, 0, 0, 0]);
 
@@ -57,10 +59,59 @@ fn apply_blur(input_path: String, output_path: String, sigma: f32) -> PyResult<(
     save_image(&gaussian_blur_f32(&img.to_rgba8(), sigma), &output_path, ImageFormat::Png)
 }
 
+fn sharpen_kernel() -> [[i32; 3]; 3] {
+    [
+        [0, -1, 0],
+        [-1, 5, -1],
+        [0, -1, 0]
+    ]
+}
+
+fn apply_convolution(image: &RgbImage) -> RgbImage {
+    let (width, height) = image.dimensions();
+    let kernel = sharpen_kernel();
+    let mut output_image: RgbImage = ImageBuffer::new(width, height);
+
+    for y in 1..height as usize - 1 {
+        for x in 1..width as usize - 1 {
+            let mut r_total = 0;
+            let mut g_total = 0;
+            let mut b_total = 0;
+
+            for ky in 0..3 {
+                for kx in 0..3 {
+                    let px = image.get_pixel((x + kx - 1) as u32, (y + ky - 1) as u32);
+                    r_total += px[0] as i32 * kernel[ky][kx];
+                    g_total += px[1] as i32 * kernel[ky][kx];
+                    b_total += px[2] as i32 * kernel[ky][kx];
+                }
+            }
+
+            let r = r_total.clamp(0, 255) as u8;
+            let g = g_total.clamp(0, 255) as u8;
+            let b = b_total.clamp(0, 255) as u8;
+ 
+ 
+            // Inside the loop
+            let x_u32: u32 = x.try_into().expect("Index out of u32 bounds");
+            let y_u32: u32 = y.try_into().expect("Index out of u32 bounds");
+            output_image.put_pixel(x_u32, y_u32, Rgb([r, g, b]));
+        }
+    }
+
+    output_image
+}
+
 #[pyfunction]
 fn apply_sharpen(input_path: String, output_path: String) -> PyResult<()> {
-    let img = open_image(&input_path)?;
-    save_image(&sharpen3x3(&img.to_luma8()), &output_path, ImageFormat::Png)
+    let img = image::open(&input_path)
+        .map_err(|e| PyErr::new::<PyIOError, _>(format!("Failed to open image: {}", e)))?
+        .to_rgb8();
+    let sharpened_img = apply_convolution(&img);
+
+    sharpened_img.save(&output_path)
+        .map_err(|e| PyErr::new::<PyIOError, _>(format!("Failed to save image: {}", e)))?;
+    Ok(())
 }
 
 #[pyfunction]
@@ -68,11 +119,18 @@ fn apply_edge_detection(input_path: String, output_path: String) -> PyResult<()>
     let img = open_image(&input_path)?;
     let edge_detected_image_u16 = sobel_gradients(&img.to_luma8());
 
+    // Find the maximum gradient value in the image
+    let max_val = edge_detected_image_u16.pixels().map(|p| p[0]).max().unwrap_or(0);
+
     let mut edge_detected_image_u8 = ImageBuffer::new(edge_detected_image_u16.width(), edge_detected_image_u16.height());
     for (x, y, pixel) in edge_detected_image_u16.enumerate_pixels() {
-        let val = pixel[0] as u32;
-        let scaled_val = (val * 255) / u16::MAX as u32;
-        edge_detected_image_u8.put_pixel(x, y, Luma([scaled_val as u8]));
+        // Scale based on the maximum gradient value instead of u16::MAX
+        let scaled_val = if max_val > 0 {
+            (pixel[0] as u32 * 255 / max_val as u32) as u8
+        } else {
+            0
+        };
+        edge_detected_image_u8.put_pixel(x, y, Luma([scaled_val]));
     }
     save_image(&edge_detected_image_u8, &output_path, ImageFormat::Png)
 }
